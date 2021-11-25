a31717ed2591e0fccf3d5899308ad2bcf9362657
Wocco <vanuytselwout@gmail.com>
2021-11-25 15:10:57 +0100

    gui initial commit

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,4 +1,20 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUI {
+    private JButton AddPeopleButton;
+    private JLabel ListOfPeople;
+
+    public GUI() {
+        AddPeopleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"hello world");
+            }
+        });
+    }
+    
 }

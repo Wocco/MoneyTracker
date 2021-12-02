042bd1b420d3dfd037867157de701744952da79e
Wocco <vanuytselwout@gmail.com>
2021-12-02 15:15:29 +0100

    added observer interface

---
+++ src/observers/Observer.java
@@ -0,0 +1,10 @@
+package observers;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public interface  Observer extends PropertyChangeListener {
+
+    @Override
+    void propertyChange(PropertyChangeEvent evt);
+}

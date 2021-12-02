602ecadf4a8a41dd98cabe73e981ea5a227a6853
Wocco <vanuytselwout@gmail.com>
2021-12-02 14:50:10 +0100

    controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,19 @@
 package Controller;
 
+import Tickets.Ticket;
+import Tickets.TicketFactory;
+
+import java.util.Date;
+
 public class Controller
 {
+
+
+    public Ticket createAirplaneTicket(TicketFactory ticketFactory,String description, int value, Date purchaseDate, boolean splitEvenly)
+    {
+        Ticket ticket = ticketFactory.makeTicket("AirplaneTicket",description,value,purchaseDate,splitEvenly);
+        //Add the ticket to the database
+        return ticket;
+    }
+
 }

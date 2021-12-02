5cd15d25a9e27e50f8c55213d9b62a6eef6b9284
Wocco <vanuytselwout@gmail.com>
2021-12-02 14:53:12 +0100

    Needs dB refrence controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -15,5 +15,30 @@ public class Controller
         //Add the ticket to the database
         return ticket;
     }
+    public Ticket createConcertTicket(TicketFactory ticketFactory,String description, int value, Date purchaseDate, boolean splitEvenly)
+    {
+        Ticket ticket = ticketFactory.makeTicket("ConcertTicket",description,value,purchaseDate,splitEvenly);
+        //Add the ticket to the database
+        return ticket;
+    }
+    public Ticket createOtherTicket(TicketFactory ticketFactory,String description, int value, Date purchaseDate, boolean splitEvenly)
+    {
+        Ticket ticket = ticketFactory.makeTicket("OtherTicket",description,value,purchaseDate,splitEvenly);
+        //Add the ticket to the database
+        return ticket;
+    }
+    public Ticket createRestaurantTicket(TicketFactory ticketFactory,String description, int value, Date purchaseDate, boolean splitEvenly)
+    {
+        Ticket ticket = ticketFactory.makeTicket("RestaurantTicket",description,value,purchaseDate,splitEvenly);
+        //Add the ticket to the database
+        return ticket;
+    }
+
+    public Ticket createTaxiTicket(TicketFactory ticketFactory,String description, int value, Date purchaseDate, boolean splitEvenly)
+    {
+        Ticket ticket = ticketFactory.makeTicket("TaxiTicket",description,value,purchaseDate,splitEvenly);
+        //Add the ticket to the database
+        return ticket;
+    }
 
 }

12646b0b04b4e9b062c842f5b8f71e54fccf8eb6
Wocco <vanuytselwout@gmail.com>
2021-12-06 15:37:29 +0100

    almost finished the add ticket gui with actionlisteners

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -10,6 +10,12 @@ public class GUI {
     private JTabbedPane Tabbedpane;
     private JLabel welcomeTextField;
     private JComboBox comboBoxAddTicket;
+    private JLabel TicketOptionsLabel;
+    private JTextField PriceOfTicketField;
+    private JLabel priceLabel;
+    private JComboBox comboBoxSplitEven;
+    private JLabel splitEvenLabel;
+    private JButton addTicketButton;
 
     /**
      * Actions for all the buttons
@@ -17,13 +23,25 @@ public class GUI {
 
     public GUI()
     {
-
+        // addticket
+        //combobox kind of ticker
         comboBoxAddTicket.addItem("AirplaneTicket");
         comboBoxAddTicket.addItem("TaxiTicket");
         comboBoxAddTicket.addItem("ConcertTicket");
         comboBoxAddTicket.addItem("OtherTicket");
         comboBoxAddTicket.addItem("RestaurantTicket");
+        //combobox split evenly or not
+        comboBoxSplitEven.addItem("Split evenly");
+        comboBoxSplitEven.addItem("Do not split");
+        /**
+         * Listener for send button when a ticket is added.
+         */
+        addTicketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
     }
 
 
